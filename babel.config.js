6318a210c4b9339f2dfe77b5cafad977bd8ad24f
mtthdz <matt@mtthdz.com>
2024-07-14 12:09:00 -0400

    (proj) added import alias

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,22 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      ['module-resolver', {
+        'alias': {
+          '@Navigation': './src/navigation',
+          '@Components': './src/components',
+          '@Screens': './src/screens',
+          '@Stores': './src/stores',
+          '@Assets': './assets'
+        },
+        'extensions': [
+          '.js',
+          '.jsx',
+          '.ts',
+          '.tsx',
+        ]
+      }],
+    ]
   };
 };
