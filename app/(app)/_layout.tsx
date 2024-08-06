09f2b1158eb72435c6b397e53ec62fd806dbad09
mtthdz <matt@mtthdz.com>
2024-08-06 19:10:22 -0400

    (ADD) signOut functionality

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -2,7 +2,7 @@ import { reduxSelect } from "@/store/types/reduxHooks";
 import { Redirect, Stack } from "expo-router";
 
 export default function AppLayout() {
-  const currentUser = reduxSelect(state => state.auth.uid);
+  const currentUser = reduxSelect(state => state.auth.id);
 
   if (!currentUser) return <Redirect href="/SignIn" />;
   return <Stack />
--- app/(app)/index.tsx
+++ app/(app)/index.tsx
@@ -1,4 +1,6 @@
+import { supabaseSignOut } from "@/features/auth/api/supabaseSignOut";
 import { authSignOut } from "@/store/actions/authActions";
+import { userMetaDelete } from "@/store/actions/userMetaActions";
 import { reduxDispatch } from "@/store/types/reduxHooks";
 import { Link } from "expo-router";
 import { Text, TouchableOpacity } from "react-native";
@@ -6,12 +8,19 @@ import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
   const dispatch = reduxDispatch();
+
+  const signOut = async () => {
+    await supabaseSignOut();
+    dispatch(authSignOut());
+    dispatch(userMetaDelete());
+  }
+
   return (
     <SafeAreaView>
-      <Text>pop</Text>
+      <Text>hello</Text>
       <Link href="/about">about</Link>
       <TouchableOpacity
-        onPress={() => dispatch(authSignOut())}
+        onPress={() => signOut()}
       >
         <Text>Sign Out</Text>
       </TouchableOpacity>
