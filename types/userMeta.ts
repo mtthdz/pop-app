be55c9d8e61bb69623911f01894bf5b32ec7dde1
mtthdz <matt@mtthdz.com>
2024-08-06 19:07:17 -0400

    (UPDATE) type naming to supabase names

--- types/userMeta.ts
+++ types/userMeta.ts
@@ -1,10 +1,10 @@
 // Used both in Supabase API Auth and UserMeta Reducer
 export interface UserMeta {
   id: null | string;
+  user_first: null | string;
+  user_last: null | string;
   user_name: null | string;
-  first_name: null | string;
-  last_name: null | string;
-  bio: null | string;
+  user_bio: null | string;
   user_picture: null | string;
   error: null | string;
 }
