b0e5acc3006261c26fed8b8bc75f4fe64b9312c3
mtthdz <matt@mtthdz.com>
2024-08-06 17:50:43 -0400

    (REFACTOR)

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -4,9 +4,6 @@ import { Redirect, Stack } from "expo-router";
 export default function AppLayout() {
   const currentUser = reduxSelect(state => state.auth.uid);
 
-  if (!currentUser) {
-    return <Redirect href="/SignIn" />;
-  }
-
+  if (!currentUser) return <Redirect href="/SignIn" />;
   return <Stack />
 }
