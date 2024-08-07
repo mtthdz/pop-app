0160c3005ddee7313ff1266fcf172a1546df6794
mtthdz <matt@mtthdz.com>
2024-08-07 17:45:43 -0400

    documentation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,19 @@
-import { Slot, Stack } from "expo-router";
+import { Slot } from "expo-router";
 import { store } from "@/store/configureStore";
 import { Provider } from "react-redux";
 
+/**
+ * Routing Map
+ * 
+ * /app
+ * |-- _layout        auth & redux store 
+ * |-- SignIn         auth screen
+ * |-- (app)          actual app
+ * |   |-- _layout    stack nav
+ * |   |-- index      main/home screen
+ * |   |-- Profile    profile screen, drawer
+ * |   |-- Chat       dynamic, own `_layout`
+ */
 export default function RootLayout() {
   return (
     <Provider store={store}>
