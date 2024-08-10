44b9b2cf453be2b0cc89f5bd58b592b40edd79c5
mtthdz <matt@mtthdz.com>
2024-08-10 14:51:37 -0400

    renamed supabase config

--- features/auth/api/supabaseSignOut.ts
+++ features/auth/api/supabaseSignOut.ts
@@ -1,4 +1,4 @@
-import { supabase } from "@/lib/supabase";
+import { supabase } from "@/lib/supabaseConfig";
 
 export const supabaseSignOut = async (): Promise<void> => {
   try {
--- features/main/api/getChatList.tsx
+++ features/main/api/getChatList.tsx
@@ -1,4 +1,4 @@
-import { supabase } from "@/lib/supabase"
+import { supabase } from "@/lib/supabaseConfig"
 import { ChatMeta } from "../types/chatMeta";
 
 /**
