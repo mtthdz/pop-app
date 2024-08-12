ae68f75152cb1bc44d382742b93dd0fd184c5453
mtthdz <matt@mtthdz.com>
2024-08-11 22:08:31 -0400

    (UPDATE) moved responsive scrollview to chat screen component

--- app/(app)/chat/[id].tsx
+++ app/(app)/chat/[id].tsx
@@ -1,5 +1,5 @@
-import { useRef } from "react";
-import { KeyboardAvoidingView, Platform, ScrollView } from "react-native";
+import { useEffect, useRef } from "react";
+import { Keyboard, KeyboardAvoidingView, Platform, ScrollView } from "react-native";
 import { useLocalSearchParams } from "expo-router";
 import { SafeAreaView, useSafeAreaInsets } from "react-native-safe-area-context";
 import ChatForm from "@/features/chat/components/ChatForm";
@@ -7,14 +7,35 @@ import ChatHeader from "@/features/chat/components/ChatHeader";
 import ChatWindow from "@/features/chat/components/ChatWindow";
 
 /**
- * TODO: scrollview ref unresponsive on render
- * TODO: scrollview ref unresponsive on textInput multiline
+ * TODO: scrollview bottom out on render not perfect. Margin/padding issue with 
+ * ChatForm underneath
+ * TODO: scrollview ref unresponsive on textInput multiline. Consider Context to
+ * re-trigger autoscroll on multiline breaks
  * TODO: TS errors
  */
 export default function Chat() {
   const params        = useLocalSearchParams();
   const insets        = useSafeAreaInsets();
   const scrollViewRef = useRef<ScrollView>(null);
+
+  useEffect(() => {
+    const keyboardDidShowListener = Keyboard.addListener('keyboardDidShow', () => {
+      if (scrollViewRef.current) {
+        scrollViewRef.current.scrollToEnd({ animated: true });
+      }
+    });
+
+    const keyboardDidHideListener = Keyboard.addListener('keyboardDidHide', () => {
+      if (scrollViewRef.current) {
+        scrollViewRef.current.scrollToEnd({ animated: true });
+      }
+    });
+
+    return () => {
+      keyboardDidShowListener.remove();
+      keyboardDidHideListener.remove();
+    };
+  }, []);
 
   return (
     <SafeAreaView
@@ -27,7 +48,15 @@ export default function Chat() {
         keyboardVerticalOffset={insets.top}
         style={{ flex: 1 }}
       >
-        <ChatWindow scrollViewRef={scrollViewRef} />
+        <ScrollView
+          ref={scrollViewRef}
+          onContentSizeChange={() => {
+            scrollViewRef.current?.scrollToEnd();
+          }}
+          // contentContainerStyle={{ paddingBottom: insets.bottom }}
+        >
+          <ChatWindow/>
+        </ScrollView>
         <ChatForm />
       </KeyboardAvoidingView>
     </SafeAreaView>
